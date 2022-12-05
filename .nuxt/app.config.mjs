61c3f3a3a593e48c22f748ba44a08201c961cb26
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-05 22:20:26 +0600

    Update Pages
    Update

--- .nuxt/app.config.mjs
+++ .nuxt/app.config.mjs
@@ -1,5 +1,5 @@
 
-import { defuFn } from '/home/poliweb/Документы/www/AthenaPlus/new/NewSiteAthenaPlus/node_modules/defu/dist/defu.mjs'
+import { defuFn } from '/home/poliweb/Документы/www/AthenaPlus/NewSiteAthenaPlus/node_modules/defu/dist/defu.mjs'
 
 const inlineConfig = {}
 
