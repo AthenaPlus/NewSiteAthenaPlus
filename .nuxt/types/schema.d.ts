0c06ed0ad445b9c9065f3335df87a09755fd797a
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-06 07:48:30 +0600

    Update Modules

--- .nuxt/types/schema.d.ts
+++ .nuxt/types/schema.d.ts
@@ -3,6 +3,7 @@ declare module '@nuxt/schema' {
   interface NuxtConfig {
     ["tailwindcss"]?: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
     ["image"]?: typeof import("@nuxt/image-edge").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["svgo"]?: typeof import("nuxt-svgo").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
     ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
   }
   interface RuntimeConfig {
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
+        <IconHome class="w-10 h-10 stroke-pink-500" />
         <h1 class="text-pink-500">NewSiteAthenaPlus</h1>
     </div>
 </template>
 
 <script setup>
-
+import IconHome from '~/assets/icons/academiccap.svg'
 </script>
 
 <style lang="scss" scoped>
