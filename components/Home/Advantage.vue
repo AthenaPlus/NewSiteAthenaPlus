c481605b6734b96bdc67bd52e7e379825a780859
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-07 10:53:22 +0600

    Add component Advantage.vue and  Update

---
+++ components/Home/Advantage.vue
@@ -0,0 +1,55 @@
+<template>
+<section class="text-gray-100 flex h-screen bg-[url('/img/bg_second_freim.webp')]">
+    <div class="container mx-auto flex px-5 pb-24 pt-60 md:flex-row flex-col items-center ">
+        <div class="w-full h-full max-w-3xl">
+            <h1 class="HeroTitle">
+                ПРЕВОСХОДСТВО ИДЕИ И ЛОГИКИ
+                </h1>
+                <h2 class="HeroSubtitle mb-2.5 mt-5">
+                    Мы являемся самой прогрессивной веб-студией на казахстанском рынке
+                </h2>
+                <p class="mb-12 text-xl LineHeight_30 font-normal">
+                    Мы создали Athena Plus объединив наши навыки веб-разработки, контент-стратегии и социальных сетей в мощную и гибкую команду. Наша команда дизайнеров, стратегов брендов и менеджеров проектов сотрудничает с вами, чтобы создать дальновидный пользовательский опыт, чтобы вы могли сосредоточиться на результате. Сотрудничество с нами гарантирует вам приобретение лучшего диджитал-продукта.
+                </p>
+                <div class="flex justify-start">
+                    <button
+                        class="inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 font_Airborne text-xl uppercase">записаться на интервью</button>
+                </div>
+        </div>
+    <!-- <div class="bg-pink-500/50 w-1/2 h-screen"></div> -->
+    </div>
+</section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.HeroTitle {
+    font-family: 'Airborne';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 100px;
+    line-height: 100%;
+    /* or 100px */
+    color: #FFFFFF;
+}
+
+.HeroSubtitle {
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 26px;
+    line-height: 150%;
+    /* or 39px */
+    color: #FFFFFF;
+}
+
+.LineHeight_30 {
+    line-height: 150%;
+}
+
+
+
+</style>
