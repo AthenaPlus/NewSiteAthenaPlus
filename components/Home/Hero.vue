821a4408958c49a0f641c21f4ebe92e048512068
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-08 11:37:34 +0600

    Update

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div class="md:w-1/2">
-                    <SvgPicterFrame1 class="object-cover object-center rounded" width="837" height="951" />
+                    <SvgPicterFrame1 class="object-cover object-center rounded"/>
                 </div>
             </div>
         </div>
