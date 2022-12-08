c61171547b07bffe5df14044c872a0ba9e516373
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-08 12:07:40 +0600

    Update Whatwedo.vue

--- components/Home/Whatwedo.vue
+++ components/Home/Whatwedo.vue
@@ -118,6 +118,7 @@ import IconIcon6 from '~/assets/icons/icon6.svg'
 
 
 
+
 const WhatwedoTitle = 'чем мы занимаемся'
 const WhatwedoDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 const WhatwedoDescription2 = 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'
