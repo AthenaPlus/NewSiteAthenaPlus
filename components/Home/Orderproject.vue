fac172839faa577311138553f107e95d73d4d286
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-09 11:38:48 +0600

    Update Orderproject.vue

--- components/Home/Orderproject.vue
+++ components/Home/Orderproject.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="text-gray-600 body-font relative">
+    <section class="text-gray-600 body-font relative min-h-screen bg-[url('/img/bg_frame4.jpg')]">
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="title">Есть проект?</h1>
+                <h1 class="title mt-16">Есть проект?</h1>
             </div>
         </div>
     </section>
