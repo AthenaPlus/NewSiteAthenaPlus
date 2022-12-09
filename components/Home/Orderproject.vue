b7e2f8c4985fffb5eebcf6c632e1b0b87e005b66
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-09 11:58:10 +0600

    Update Orderproject.vue

--- components/Home/Orderproject.vue
+++ components/Home/Orderproject.vue
@@ -3,15 +3,30 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-12">
                 <h1 class="title mt-16">Есть проект?</h1>
+                <div class="flex items-center justify-end gap-8 mr-32">
+                    <IconArrow/>
+                    <h2 class="subtitle">Оставте заявку</h2>
+                </div>
             </div>
         </div>
     </section>
 </template>
 
 <script setup>
+import IconArrow from '~/assets/icons/arrow.svg'
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.subtitle {
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 90px;
+    line-height: 113px;
+    text-align: right;
 
+    color: #FFFFFF;
+
+}
 </style>
