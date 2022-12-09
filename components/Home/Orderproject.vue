d8d34ef1d5c0cb499da4a39295ab1fcb4fa4b977
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-09 13:47:58 +0600

    Update Orderproject.vue

--- components/Home/Orderproject.vue
+++ components/Home/Orderproject.vue
@@ -7,6 +7,45 @@
                     <IconArrow/>
                     <h2 class="subtitle">Оставте заявку</h2>
                 </div>
+                <div class="flex flex-row">
+                    <div class="">
+                        <h3 class="text-left text-2xl text-white Source_Sans_Pro mb-8">Какую услуги вы выберите?</h3>
+                        <div class="flex gap-5">
+                            <button v-for="batton in battons" class="btn text-white">
+                                    {{ batton.title }}
+                            </button>
+                        </div>
+                        <form action="" class="flex flex-wrap mt-10">
+                            <div class="p-2 w-1/2">
+                                <div class="relative">
+                                    <input type="text" id="name" name="name" placeholder="Ваше имя"
+                                        class="w-full bg-gray-100 bg-opacity-10  border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2">
+                                <div class="relative">
+                                    <input type="email" id="email" name="email" placeholder="Ваш телефон"
+                                        class="w-full bg-gray-100 bg-opacity-10  border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-full">
+                                <div class="relative">
+                                    <textarea id="message" name="message" placeholder="Кратко опишите проект"
+                                        class="w-full bg-gray-100 bg-opacity-10 border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                   
+                                </div>
+                            </div>
+                            <div class="p-2">
+                                <button
+                                    class="btn-send shadow-pink-500/50 flex mx-auto text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg">Оставить заявку</button>
+
+                            </div>
+                        </form>
+                    </div>
+                    <div class="">
+                        
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -14,6 +53,21 @@
 
 <script setup>
 import IconArrow from '~/assets/icons/arrow.svg'
+
+const battons = [
+    {
+        title: 'веб-сайт'
+    },
+    {
+        title: 'приложение'
+    },
+    {
+        title: 'графика'
+    },
+    {
+        title: 'smm'
+    }
+]
 
 </script>
 
@@ -28,5 +82,26 @@ import IconArrow from '~/assets/icons/arrow.svg'
 
     color: #FFFFFF;
 
+}
+
+.btn {
+    width: 200px;
+    height: 50px;
+
+    font-family: 'Airborne';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+
+    background: rgba(11, 11, 11, 0.1);
+    border: 2px solid #FFFFFF;
+    backdrop-filter: blur(2px);
+}
+
+.btn-send:hover {
+    background: linear-gradient(180deg, #FCB040 0%, #FF2BFF 100%);
+    box-shadow: 0 20px 25px 5px rgba(248, 4, 220, 0.1), 0 10px 18px -6px rgba(237, 161, 8, 0.1);
 }
 </style>
