5ecd337247ecd210002d356d882dd605749b6db4
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-09 11:25:06 +0600

    Update

---
+++ components/Home/Orderproject.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center w-full mb-12">
+                <h1 class="title">Есть проект?</h1>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
