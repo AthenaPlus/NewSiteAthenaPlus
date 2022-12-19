52bb518c29128fa81cc0238053be5dd7847845e1
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-19 08:52:44 +0600

    Update servicesprices.vue

--- pages/servicesprices.vue
+++ pages/servicesprices.vue
@@ -1,7 +1,43 @@
 <template>
-    <section class="text-gray-600 body-font">
+    <section class="text-white body-font">
         <div class="container px-5 py-24 mx-auto">
-            <h1 class="title">ПРивет это страница Services and Prices!</h1>
+            <!-- Blok Header -->
+            <div class="border-b-2 border-white pb-[60px]">
+                <h1 class="text-4xl sm:text-6xl md:text-[86px] text-white">Что мы предлагаем</h1>
+                <p class="text-2xl sm:text-3xl text-white md:leading-[45px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+            </div>
+            <!-- End Blok Header -->
+            <!-- Blok Веб-Сайты -->
+            <div class="mt-[60px] flex flex-row flex-wrap">
+                <div class="w-full md:w-1/4">
+                    <h2 class="font_Airborne text-4xl md:text-6xl uppercase">Веб-Сайты</h2>
+                </div>
+                <div class="w-full md:w-3/4 flex flex-row flex-wrap">
+                    <div class="w-full lg:w-1/2 p-8">
+                        <h3 class="text-3xl md:text-[32px] mb-2.5 Source_Sans_Pro">Landing page</h3>
+                        <p class="text-lg">Одностраничный сайт (Landing Page) – так называют промо-сайты для презентации товара, услуги и организации мгновенных продаж. Одна-единственная страница принесёт колоссальную прибыль: иначе и быть не может.</p>
+                        <button class="w-full py-[10px] text-right text-2xl">Заказать</button>
+                    </div>
+                    <div class="w-full lg:w-1/2 p-8">
+                        <h3 class="text-3xl md:text-[32px] mb-2.5 Source_Sans_Pro">Интернет-магазин</h3>
+                        <p class="text-lg">Одностраничный сайт (Landing Page) – так называют промо-сайты для презентации товара, услуги и организации мгновенных продаж. Одна-единственная страница принесёт колоссальную прибыль: иначе и быть не может.</p>
+                        <button class="w-full py-[10px] text-right text-2xl">Заказать</button>
+                    </div>
+                    <div class="w-full lg:w-1/2 p-8">
+                        <h3 class="text-3xl md:text-[32px] mb-2.5 Source_Sans_Pro">Сайт-визитка</h3>
+                        <p class="text-lg">Одностраничный сайт (Landing Page) – так называют промо-сайты для презентации товара, услуги и организации мгновенных продаж. Одна-единственная страница принесёт колоссальную прибыль: иначе и быть не может.</p>
+                        <button class="w-full py-[10px] text-right text-2xl">Заказать</button>
+                    </div>
+                    <div class="w-full lg:w-1/2 p-8">
+                        <h3 class="text-3xl md:text-[32px] mb-2.5 Source_Sans_Pro">Индивидуальный сайт</h3>
+                        <p class="text-lg">Одностраничный сайт (Landing Page) – так называют промо-сайты для презентации товара, услуги и организации мгновенных продаж. Одна-единственная страница принесёт колоссальную прибыль: иначе и быть не может.</p>
+                        <button class="w-full py-[10px] text-right text-2xl">Заказать</button>
+                    </div>
+                </div>
+            </div>
+            <!-- End Blok Веб-Сайты -->
         </div>
     </section>
 </template>
