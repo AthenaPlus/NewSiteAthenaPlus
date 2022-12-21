298aed5d87fb081bd6d6880f7dd0c3edf7f91e41
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2022-12-21 06:16:59 +0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,12 @@
 </template>
 
 <script setup>
-
+useHead({
+    title: 'Athena My App',
+    meta: [
+        { name: 'description', content: 'веб-проекты со стилем Мы создаём креативные веб-проекты для вашего бизнеса' }
+    ],
+})
 </script>
 
 <style lang="scss" scoped>
