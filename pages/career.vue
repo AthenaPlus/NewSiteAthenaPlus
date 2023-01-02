8e2713c5faa5f650c904a7f2489ed8437180cbed
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-02 13:51:03 +0600

    Update Career.vue and AboutMenu.vue

--- pages/career.vue
+++ pages/career.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-
-    </div>
+    <section class="text-white body-font">
+        <div class="container flex flex-wrap px-5 py-24 mx-auto">
+            <div class="w-full md:w-1/2 mb-10 md:mb-0">
+                <h1 class="sticky top-20 text-4xl md:text-6xl lg:text-[66px]">Построй карьеру вместе с Athena Plus</h1>
+            </div>
+            <div class="w-full md:w-1/2">
+                <img src="/img/career.png" alt="" class="max-w-full">
+                <p class="my-4 max-w-[600px]">
+                    Athena Plus это Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                </p>
+                <button class="btn-primary hover:btn--shadow">Подать заявку</button>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <style scoped>
