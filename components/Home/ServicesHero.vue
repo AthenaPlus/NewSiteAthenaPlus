01b8cf8d5485e2e1f94b1ad75e2c1ac5991c6cf5
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-05 21:25:30 +0600

    Update ServicesHero.vue

--- components/Home/ServicesHero.vue
+++ components/Home/ServicesHero.vue
@@ -2,7 +2,6 @@
     <section class="text-white relative min-h-screen bg-[url('/img/bg_ServicesHero.webp')] bg-no-repeat bg-top bg-cover">
         <div class="container px-5 py-24 mx-auto">
             <div class="mt-11 w-full lg:w-[700px]">
-                <span class="text-xl leading-6 plan">Услуги и цены</span>
                 <h2 class="font_Airborne text-4xl md:text-7xl lg:text-[80px] leading-[92px] my-5 uppercase">Каждый проект</h2>
                 <h2 class="font_Airborne uppercase text-2xl md:text-4xl lg:text-[50px] leading-[57px] text-right pr-6 mb-[30px]">как арт-объект</h2>
                 <p class="w-full lg:w-[470px] text-lg">Мы крайне негативно относимся к сайтам-однодневкам с кривым дизайном и слабой идеей. Поэтому для нас важно предать вашему проекту уникальную айдентику.</p>
