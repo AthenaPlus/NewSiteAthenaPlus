cca443734b0070316302b0a0bfac6ccc1d47a5b5
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-06 17:29:24 +0600

    Add BG for Home/Orderproject

--- components/Home/Orderproject.vue
+++ components/Home/Orderproject.vue
@@ -1,28 +1,29 @@
 <template>
-    <section class="text-white body-font relative min-h-screen bg-center bg-cover bg-[url('/img/bg_frame4.jpg')] z-0">
-        <div class="container px-5 py-24 mx-auto">
+    <section class="text-white body-font relative min-h-screen bg-top bg-contain bg-no-repeat bg-[url('/img/bg_frame4_mob.png')] md:bg-center md:bg-cover  md:bg-[url('/img/bg_frame4.jpg')] z-0">
+        <div class="container px-5 py-16  md:py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="SuperTitle text-left mt-16">Есть проект?</h1>
-                <div class="flex items-center justify-end gap-8 mr-32">
+                <h1 class="text-[32px] SuperTitle text-left md:mt-16">Есть проект?</h1>
+                <div class="flex items-center md:justify-end gap-2  md:mr-32">
                     <IconArrow class="hidden md:block"/>
-                    <h2 class="text-4xl md:text-5xl lg:text-7xl">Оставьте заявку</h2>
+                    <h2 class="text-xl font_Airborne md:Source_Sans_Pro md:text-5xl lg:text-7xl">Оставьте заявку</h2>
+                    <IconArrowright class="block md:hidden w-10" />
                 </div>
-                <div class="flex flex-col md:flex-row items-start md:items-center w-full">
+                <div class="flex flex-col md:flex-row items-start md:items-center w-full mt-">
                     <div class="">
-                        <h3 class="text-left text-2xl text-white Source_Sans_Pro mb-8">Какую услуги вы выберите?</h3>
-                        <div class="flex flex-wrap justify-start  gap-5">
+                        <h3 class="hidden md:block text-left text-2xl text-white Source_Sans_Pro mb-8">Какую услуги вы выберите?</h3>
+                        <div class="hidden md:flex flex-wrap justify-start  gap-5">
                             <button v-for="batton in battons" class="btn--label text-white">
                                     {{ batton.title }}
                             </button>
                         </div>
                         <form action="" class="flex flex-wrap my-10">
-                            <div class="pr-5 w-1/2">
+                            <div class="pr-5 w-full md:w-1/2 mb-5 md:mb-0">
                                 <div class="relative">
                                     <input type="text" id="name" name="name" placeholder="Ваше имя"
                                         class="w-full bg-gray-100 bg-opacity-5  border-b-2 border-gray-300 focus:border-pink-500 focus:border-b-2 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
                             </div>
-                            <div class="pr-5 w-1/2">
+                            <div class="pr-5 w-full md:w-1/2">
                                 <div class="relative">
                                     <input type="tel" id="phone" name="phone" placeholder="Ваш телефон"
                                     class="w-full bg-gray-100 bg-opacity-5  border-b-2 border-gray-300 focus:border-pink-500 focus:border-b-2 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
@@ -56,6 +57,7 @@
 
 <script setup>
 import IconArrow from '~/assets/icons/arrow.svg'
+import IconArrowright from '~/assets/icons/arrow__right.svg'
 
 
 const battons = [
@@ -80,7 +82,6 @@ const battons = [
     font-family: 'Airborne';
     font-style: normal;
     font-weight: 400;
-    font-size: 36px;
     line-height: 40px;
 
     color: #FFFFFF;
