08b8cb62805f9df29326729833191016ce1f2aa2
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-07 14:23:20 +0600

    Update Hero.vue

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -4,23 +4,28 @@
             <div class=" flex  lg:flex-row flex-col items-center">
                 <div
                     class="order-2 lg:order-1 lg:flex-grow w-full lg:w-1/2 lg:pr-16 md:pr-14 flex flex-col md:items-start text-left mb-16 md:mb-0 items-center">
-                    <h1 class="hidden lg:block h1-title animate__animated animate__bounce">
+                    <h1 class="hidden lg:block h1-title animate__animated animate__bounceInRight">
                         {{ HeroTitle }}
                     </h1>
+
                     <h2 class="text-xl lg:text-[26px] font-semibold mb-4 mt-0 md:mt-5 mr-auto">{{ HeroSubtitle }}</h2>
-                    <p class="hidden md:contents mb-12 text-base lg:text-xl LineHeight_30 font-normal">{{ HeroDescription }}</p>
+                    <p class="hidden md:contents mb-12 text-base lg:text-xl LineHeight_30 font-normal">{{
+                        HeroDescription
+                    }}</p>
                     <div class="mr-auto md:mt-[50px]">
-                        <button
-                            class="btn btn--shadow ">записаться
+                        <button class="btn btn--shadow">записаться
                             на интервью</button>
                     </div>
                     <div class="hidden md:flex flex-wrap justify-start gap-2.5 mt-28">
                         <Nuxt-link v-for="HeroLabel in HeroLabels" :key="HeroLabel.index" :to="HeroLabel.url"
-                            class="font_Airborne rounded-full border-2 border-white py-5 px-6">{{ HeroLabel.title }}</Nuxt-link>
+                            class="font_Airborne rounded-full border-2 border-white py-5 px-6">{{
+                                HeroLabel.title
+                            }}</Nuxt-link>
                     </div>
                 </div>
                 <div class="order-1 lg:order-2 w-full lg:w-1/2">
-                    <h1 class="contents lg:hidden font_Airborne text-[30px] md:text-[40px] leading-none animate__animated animate__bounce">
+                    <h1
+                        class="contents lg:hidden font_Airborne text-[30px] md:text-[40px] leading-none _anim-items animat_Title">
                         {{ HeroTitle }}
                     </h1>
                     <div class="pt-5 lg:pt-0">
@@ -129,6 +134,32 @@ const HeroLabels = [
     border-image: linear-gradient(to bottom, #FF2BFF, #FCB040  );
     border-image-slice: 1;
     transition: all 0.5s ease-out;
+}
+
+
+/* CSS для Анимации Заголовка и под заголовка */
+
+.animat_Title {
+	transform: translate(0px, 120%);
+	opacity: 0;
+	transition: all 0.5s ease 0s;
+}
+
+.animat_SubTitle {
+	transform: translate(0px, 100px);
+	opacity: 0;
+	transition: all 0.8s ease 0.5s;
+	--animate-duration: 0.5s;
+}
+
+.animat_Title._active {
+	transform: translate(0px, 0);
+	opacity: 1;
+}
+
+.animat_SubTitle._active {
+	transform: translate(0px, 0);
+	opacity: 1;
 }
 
 </style>
