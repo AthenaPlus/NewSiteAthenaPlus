c461935c555b2334679f38a4b9e9f245ff1a9609
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-12 14:57:55 +0600

    Update referral.vue

--- pages/referral.vue
+++ pages/referral.vue
@@ -2,11 +2,11 @@
     <section class="text-white body-font overflow-hidden bg-[url('/img/bg_referral.jpg')] bg-no-repeat bg-center bg-cover relative z-[5]">
         <div class="absolute bg-[url('/img/bg_referral_mask.png')] inset-0 -z"></div>
         <div class="container px-5 py-24 mx-auto grid min-h-screen place-content-center ">
-            <div class="flex flex-wrap ">
+            <div class="flex flex-wrap">
                 <div class="w-full md:w-1/2 mb-10 md:mb-0">
                     <h1 class="text-4xl md:text-6xl lg:text-[66px]">Стань партнером новой веб-студии </h1>
                 </div>
-                <div class="w-full md:w-1/2">
+                <div class="w-full md:w-1/2 pl-0 md:pl-10">
                     <p class="text-[26px] mb-4">
                         Приведи клиента и получи до 10% от суммы договора. Без дополнительных комиссий и подводных камней.
                     </p>
