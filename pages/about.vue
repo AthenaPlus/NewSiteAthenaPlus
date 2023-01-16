4937985af476ac38d85610d6fcca6b75a4eb50d4
KobeWanAthena <113441500+KobeWanAthena@users.noreply.github.com>
2023-01-16 15:36:50 +0600

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -17,7 +17,7 @@
             <!-- Блок  НАШЕ АГЕНСТВО -->
             <div class="flex flex-col lg:flex-row gap-10 border-b-2 border-white/70 pt-[50px] pb-10">
                 <div class="w-full lg:w-1/3">
-                    <h3 class="sticky top-10 uppercase font_Airborne text-[30px] md:text-[50px] leading-normal">Наше агенство</h3>
+                    <h3 class="sticky top-10 uppercase font_Airborne text-[30px] md:text-[50px] leading-tight">Наше агенство</h3>
                 </div>
                 <div class="w-full lg:w-2/3">
                     <div class="grid grid-flow-col-1 md:grid-cols-2  gap-[75px]">
