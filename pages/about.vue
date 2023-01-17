8285a5be2fe4c3086b43eae8aa53d29a1be010b8
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-17 08:20:41 +0600

    Update Add Slaider Swiper for pages/about

--- pages/about.vue
+++ pages/about.vue
@@ -101,18 +101,35 @@
                 </div>
             </div>
             <!-- Блок Портфолио Команды -->
-            <div class="flex flex-wrap -m-4 border-b-2 border-white/70 pt-[50px] pb-10">
-                <div v-for="employee in employees" :key="employee.index" class="p-4 lg:w-1/4 md:w-1/2">
+            <swiper @swiper="onSwiper" @slideChange="onSlideChange" :breakpoints="{
+                  '640': {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  '768': {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                  },
+                  '1024': {
+                    slidesPerView: 3,
+                    spaceBetween: 50,
+                  },
+                  '1535': {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                  }
+                }" class="flex flex-wrap -m-4 border-b-2 border-white/70 pt-[50px] pb-10 swiper">
+                <swiper-slide v-for="employee in employees" :key="employee.index" class="p-4 ">
                     <div class="h-full flex flex-col items-center text-center">
                         <img alt="team" class="flex-shrink-0 rounded-lg w-full h-[300px] object-cover object-center mb-4"
                             :src="employee.urlImg">
                         <div class="w-full">
-                            <h2 class="title-font font-medium text-lg text-white">{{  employee.name  }}</h2>
+                            <h2 class="title-font font-medium text-lg text-white">{{ employee.name }}</h2>
                             <h3 class="text-gray-500 mb-3">{{ employee.job }}</h3>
                             <p class="mb-4">
                                 {{ employee.description }}
                             </p>
-                            <span class="inline-flex">
+                            <!-- <span class="inline-flex">
                                 <a class="text-gray-500">
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -135,11 +152,11 @@
                                         </path>
                                     </svg>
                                 </a>
-                            </span>
+                            </span> -->
                         </div>
                     </div>
-                </div>
-            </div>
+                </swiper-slide>
+            </swiper>
             <!-- ======================= -->
         </div>
     </section>
@@ -218,8 +235,72 @@ const employees = [
         job: 'Маркетолог',
         description: 'Digital-Гений, оптимизирует рекламу любого направления.',
         urlImg: 'https://dummyimage.com/200x200'
+    },
+    {
+        name: 'Сотрудник 5',
+        job: 'Дизайн',
+        description: 'Holisticly negotiate reliable services without.',
+        urlImg: 'https://dummyimage.com/200x200'
+    },
+    {
+        name: 'Сотрудник 6',
+        job: 'Дизайн',
+        description: 'Objectively impact magnetic core competencies.',
+        urlImg: 'https://dummyimage.com/200x200'
+    },
+    {
+        name: 'Сотрудник 7',
+        job: 'Дизайн',
+        description: 'Progressively synergize seamless initiatives vis-a-vis.',
+        urlImg: 'https://dummyimage.com/200x200'
+    },
+    {
+        name: 'Сотрудник 8',
+        job: 'Дизайн',
+        description: 'Rapidiously disseminate scalable methodologies via.',
+        urlImg: 'https://dummyimage.com/200x200'
+    },
+    {
+        name: 'Сотрудник 9',
+        job: 'Маркетолог',
+        description: 'Continually evisculate intermandated communities through.',
+        urlImg: 'https://dummyimage.com/200x200'
     }
 ]
+
+
+
+</script>
+
+
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+// Import Swiper styles
+import 'swiper/css';
+
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+            
+        };
+        const onSlideChange = () => {
+            console.log('slide change');
+        };
+
+        return {
+
+            onSwiper,
+            onSlideChange,
+        };
+    },
+};
 
 </script>
 
---
+++ pages/test.vue
@@ -0,0 +1,43 @@
+<template>
+    <section>
+        <div class="container px-5 py-24 mx-auto text-white">
+            <swiper :slides-per-view="4" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
+                <swiper-slide>Slide 1</swiper-slide>
+                <swiper-slide>Slide 2</swiper-slide>
+                <swiper-slide>Slide 3</swiper-slide>
+                <swiper-slide>Slide 4</swiper-slide>
+                <swiper-slide>Slide 5</swiper-slide>
+                <swiper-slide>Slide 6</swiper-slide>
+                <swiper-slide>Slide 7</swiper-slide>
+                <swiper-slide>Slide 8</swiper-slide>
+                <swiper-slide>Slide 9</swiper-slide>
+            </swiper>
+        </div>
+    </section>
+</template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+      };
+    },
+  };
+</script>
