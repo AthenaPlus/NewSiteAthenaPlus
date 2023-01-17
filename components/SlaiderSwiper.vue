1fb74a23ba7147cf1b8a8ea91a53e93e2ae525b6
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-17 09:30:51 +0600

    Update SlaiderSwiper.vue

--- components/SlaiderSwiper.vue
+++ components/SlaiderSwiper.vue
@@ -2,25 +2,50 @@
     <section>
         <div class="container px-5 py-24 mx-auto text-white">
             <swiper :slides-per-view="4" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
-                <swiper-slide>Slide 1</swiper-slide>
-                <swiper-slide>Slide 2</swiper-slide>
-                <swiper-slide>Slide 3</swiper-slide>
-                <swiper-slide>Slide 4</swiper-slide>
-                <swiper-slide>Slide 5</swiper-slide>
-                <swiper-slide>Slide 6</swiper-slide>
-                <swiper-slide>Slide 7</swiper-slide>
-                <swiper-slide>Slide 8</swiper-slide>
-                <swiper-slide>Slide 9</swiper-slide>
+                <swiper-slide v-for="employee in employees" :key="employee.index">{{ employee.title }}</swiper-slide>
             </swiper>
         </div>
     </section>
 </template>
+<script setup>
+ const employees = [
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    },
+    {
+      title: 'Slaide'
+    }
+  ]
+</script>
+
 <script>
   // Import Swiper Vue.js components
   import { Swiper, SwiperSlide } from 'swiper/vue';
 
   // Import Swiper styles
   import 'swiper/css';
+
 
   export default {
     components: {
