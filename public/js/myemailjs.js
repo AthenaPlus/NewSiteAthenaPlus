b04e3b4a9d2bb590ddc29f6b70c99a2f8a82014e
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-19 09:39:18 +0600

    Update myemailjs.js

--- public/js/myemailjs.js
+++ public/js/myemailjs.js
@@ -10,7 +10,7 @@
      document.getElementById('form').addEventListener('submit', function (event) {
          event.preventDefault();
          // generate a five digit number for the contact_number variable
-         // this.contact_number.value = Math.random() * 100000 | 0;
+        //  this.contact_number.value = Math.random() * 100000 | 0;
          // these IDs from the previous steps
          emailjs.sendForm('service_y5rx3nf', 'template_ef05pji', this)
              .then(function () {
