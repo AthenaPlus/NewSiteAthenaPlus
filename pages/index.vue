68c380928e74f6cfd8f79ab7f09c0a402c411b93
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-20 12:04:34 +0600

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,17 @@
 <template>
-    <div class="scroll__y">
-        <HomeHero class="scroll-my-0 snap-start min-h-screen"/>
-        <HomeAdvantage class="scroll-my-0 snap-start min-h-screen"/>
-        <HomeServicesHero class="scroll-my-0 snap-start min-h-screen" />
-        <HomeOrderproject class="scroll-my-0 snap-start min-h-screen"/>
+    <div>
+        <section>
+            <div class="container px-5 py-24 mx-auto">
+                <h1 class="text-5xl text-white">Athena Plus</h1>
+            </div>
+        </section>
     </div>
 </template>
 
 <script setup>
 
-useHead({
-    title: 'Athena Веб-проекты со стилем',
-    meta: [
-        { name: 'description', content: 'веб-проекты со стилем Мы создаём креативные веб-проекты для вашего бизнеса' }
-    ],
-})
 </script>
 
-<script>
-export default {
+<style lang="scss" scoped>
 
-}
-</script>
-
-<style>
-    .scroll__y {
-        scroll-snap-type: y mandatory;
-    }
 </style>
