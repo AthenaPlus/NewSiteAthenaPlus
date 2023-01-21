434c36a1af17b4dbdaf1cb03d0dd4fdc81039737
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-21 16:24:21 +0600

    Update SlaiderTeam.vue

--- components/SlaiderTeam.vue
+++ components/SlaiderTeam.vue
@@ -23,9 +23,12 @@
     :modules="modules"
      class="flex flex-wrap -m-4 border-b-2 border-white/70 pt-[50px] pb-10 swiper">
         <swiper-slide v-for="employee in employees" :key="employee.index" class="p-4 ">
-            <div class="h-full flex flex-col items-center text-center">
-                <img alt="team" class="flex-shrink-0 rounded-lg w-full h-[300px] object-cover object-center mb-4"
+            <div class="h-full flex flex-col items-center text-center face">
+                <div class="relative overflow-hidden h-[300px]  mb-4">
+                    <div class="absolute bg-pink-500 rounded-lg bg-center bg-no-repeat bg--face" :class="employee.bgImg"></div>
+                    <img alt="team" class="flex-shrink-0 rounded-lg w-full h-full object-cover object-center "
                     :src="employee.urlImg">
+                </div>
                 <div class="w-full">
                     <h2 class="title-font font-medium text-lg text-white">{{ employee.name }}</h2>
                     <h3 class="text-gray-500 mb-3">{{ employee.job }}</h3>
@@ -85,49 +88,57 @@ export default {
                     name: 'Санжар',
                     job: 'Генеральный Директор',
                     description: 'Основатель Веб-Студии. Универсал. Фанат Web-Production.',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,g_face,h_300,w_300/v1674285787/team/sanjar_fyn1n2.webp',
+                    bgImg: 'sanjar'
                 },
                 {
                     name: 'Даурен',
                     job: 'Креативный Директор',
                     description: 'Со-Основатель Веб-Студии. Дизайнер. Стоял за дизайном крупных финтехов.',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://dummyimage.com/200x200',
+                    bgImg: 'dauren'
                 },
                 {
                     name: 'Павел',
                     job: 'Ведущий-Разработчик',
                     description: 'Цифровой Пионер Frontend-разработки со стажем более 20 лет.',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,e_auto_contrast,g_face,h_300,w_300/v1674285791/team/pavel_xs7tc7.webp',
+                    bgImg: 'pavel'
                 },
                 {
                     name: 'Али-Султан',
                     job: 'Маркетолог',
                     description: 'Digital-Buyer, оптимизирует рекламу любого направления.',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://dummyimage.com/200x200',
+                    bgImg: 'alisultan'
                 },
                 {
                     name: 'Камила',
                     job: 'UX/UI-артист',
                     description: 'Автор качественного UX/UI и крутых анимаций.',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,g_face,h_300,w_300/v1674285787/team/kamila_fu7que.webp',
+                    bgImg: 'kamila'
                 },
                 {
                     name: 'Атия',
                     job: 'Графический Дизайнер',
                     description: 'Солнышко компании, автор стильных логотипов и нашего Instagram',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://dummyimage.com/200x200',
+                    bgImg: 'atiy'
                 },
                 {
                     name: 'Владимир',
                     job: 'Менеджер',
                     description: 'Оптимизатор всех бизнес-процессов, от B2B до B2C',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://dummyimage.com/200x200',
+                    bgImg: ''
                 },
                 {
                     name: 'Алексей',
                     job: 'Дизайн',
                     description: 'Мощный Fullstack-разработчик. Авторствует технически тяжелые проекты.',
-                    urlImg: 'https://dummyimage.com/200x200'
+                    urlImg: 'https://dummyimage.com/200x200',
+                    bgImg: ''
                 }
             ]
         }
@@ -152,6 +163,39 @@ export default {
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.bg--face {
+    width: 0%;
+    height: 0%;
+    top: 50%;
+    left: 50%;
+    transition: all 0.4s ease-in-out;
+}
 
+.face:hover .bg--face {
+    width: 100%;
+    height: 100%;
+    top: 0%;
+    left: 0%;
+
+}
+
+.sanjar {
+    background-image: url(https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,g_face,h_300,w_300/v1674285793/team/sanjar2_oawbpy.webp);
+}
+.dauren {
+    background-image: url();
+}
+.pavel {
+    background-image: url(https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,e_auto_contrast,g_face,h_300,w_300/v1674285791/team/pavel2_pu8ba0.webp);
+}
+.alisultan {
+    background-image: url();
+}
+.kamila {
+    background-image: url(https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,g_face,h_300,w_300/v1674285787/team/kamila2_bnrhxy.webp);
+}
+.atiy {
+    background-image: url(https://res.cloudinary.com/dfevgyt2k/image/upload/c_fill,g_face,h_300,w_300/v1674285787/team/atiy2_had8rp.webp);
+}
 </style>
