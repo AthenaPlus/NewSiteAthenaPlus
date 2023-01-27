ee6acf1a63275e45a114ff9979bb3d131b569c87
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-27 09:45:05 +0600

    Update Popup.vue

--- components/Popup.vue
+++ components/Popup.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="popup mx-auto">
-        <div class="popup-header flex flex-row items-center justify-between p-3">
-            <span>{{ popupTitle }}</span>
-            <button @click="closePopup">
-                <XMarkIcon class="w-6 h-6"/>
-            </button>
-        </div>
-        <div class="popup-content">
-            <slot></slot>
-        </div>
-        <div class="popup-footer flex flex-row items-center justify-end p-3">
-            <button @click="closePopup"  class="bg-pink-500 text-white px-6 py-2">Закрыть</button>
+    <div class="popup_wrapper absolute inset-0 flex justify-center items-center bg-slate-900/90" ref="popup_wrapper">
+        <div class="popup mx-auto">
+            <div class="popup-header flex flex-row items-center justify-between p-3">
+                <span>{{ popupTitle }}</span>
+                <button @click="closePopup">
+                    <XMarkIcon class="w-6 h-6" />
+                </button>
+            </div>
+            <div class="popup-content">
+                <slot></slot>
+            </div>
+            <div class="popup-footer flex flex-row items-center justify-end p-3">
+                <button @click="closePopup" class="bg-pink-500 text-white px-6 py-2">Закрыть</button>
+            </div>
         </div>
     </div>
 </template>
@@ -31,12 +33,23 @@ export default {
         closePopup() {
             this.$emit('closePopup')
         }
+    },
+    mounted() {
+        let vm = this
+        document.addEventListener('click', function(item){
+            if (item.target === vm.$refs['popup_wrapper']){
+                vm.closePopup()
+            }
+        })
     }
 }
 
 </script>
 
 <style scoped>
+    .popup_wrapper {
+        z-index: 100000;
+    }
     .popup {
         padding: 16px;
         position: fixed;
