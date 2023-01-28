644d21dcd281371ea44b0088df3c6001a84bea60
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-28 15:51:02 +0600

    Update Whatwedo.vue

--- components/Servicesprices/Whatwedo.vue
+++ components/Servicesprices/Whatwedo.vue
@@ -120,7 +120,7 @@
                 <input type="tel" id="tel" name="tel" placeholder="Ваш телефон*" required  class="input"  />
                 <input type="email" id="email" name="email" placeholder="Ваш e-mail*" required  class="input" />
                 <textarea id="message" name="message" placeholder="Ваш вопрос*" required  class="input h-32"></textarea>
-                <input type="hidden" name="_subject" value="Новое сообщение с сайта Athenaplus.kz"/>
+                <input type="hidden" name="_subject" value="Новое сообщение с сайта Athenaplus.kz/servicesprices Форма - Задать вопрос"/>
                 <input type="hidden" name="_captcha" value="false"/>
                 <input type="text" name="_honey" style="display:none"/>
                 <input type="hidden" name="_template" value="table"/>
