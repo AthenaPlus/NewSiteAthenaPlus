1a94ccc0f507ee667f896f51ec67dbff74411f91
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-28 14:54:25 +0600

    Update Form on Whatwedo and Add page thanks.vue

---
+++ pages/thanks.vue
@@ -0,0 +1,16 @@
+<template>
+<section>
+    <div class="container px-5 py-24 mx-auto h-screen flex flex-col justify-center items-center">
+        <h1 class="text-white text-5xl  md:text-8xl">Спасибо!</h1>
+        <h2 class="text-white text-xl md:text-5xl">Ваше сообщение отправлено!</h2>
+    </div>
+</section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
