afa7ed0b5fdc532f0b00caa083646c02ee6cca5d
AthenaPlus <119859429+AthenaPlus@users.noreply.github.com>
2023-01-30 15:59:40 +0600

    Update E-mail

--- components/Servicesprices/Whatwedo.vue
+++ components/Servicesprices/Whatwedo.vue
@@ -115,7 +115,7 @@
             <SlaiderWhatwedo class="block lg:hidden" />
         </div>
         <Popup v-if="isInfoPopupVisible" popupTitle="Форма обратной связи" @closePopup="closeInfoPopup" class="fixed">
-            <form action="https://formsubmit.co/sain.athenaplus@gmail.com" method="POST" class="w-full animate__animated animate__backInDown">
+            <form action="https://formsubmit.co/24c79e3d6353b67ceddcc79963875302" method="POST" class="w-full animate__animated animate__backInDown">
                 <input type="text" id="name" name="name" placeholder="Ваше имя*" required  class="input" />
                 <input type="tel" id="tel" name="tel" placeholder="Ваш телефон*" required  class="input"  />
                 <input type="email" id="email" name="email" placeholder="Ваш e-mail*" required  class="input" />
