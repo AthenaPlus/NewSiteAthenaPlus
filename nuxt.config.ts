bd04a3adfa0c12ea6e000807c800a7a22217d0d4
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-01-31 16:41:12 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge', 'nuxt-svgo', 'nuxt-headlessui', '@fullpage/nuxt-fullpage'],
-    css: ['animate.css']
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge', 'nuxt-svgo', 'nuxt-headlessui', '@fullpage/nuxt-fullpage', 'nuxt-simple-sitemap',],
+    css: ['animate.css'],
+
+    sitemap: {
+        hostname: 'https://athenaplus.kz/',
+      },
 
 
 })
