123e002396b0e97fd86c6a1f547d37ca48d68330
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-02-01 13:37:34 +0600

    Update vacancy.vue

--- pages/vacancy.vue
+++ pages/vacancy.vue
@@ -28,7 +28,7 @@
                 <input type="tel" id="tel" name="tel" placeholder="Ваш телефон*" required  class="input"  />
                 <input type="email" id="email" name="email" placeholder="Ваш e-mail*" required  class="input" />
                 <textarea id="message" name="message" placeholder="На какую вакансию заявка?*" required  class="input h-32"></textarea>
-                <input type="hidden" name="_subject" value="Новое сообщение с сайта Athenaplus.kz/servicesprices Форма - Запрос на вакансию"/>
+                <input type="hidden" name="_subject" value="Новое сообщение с сайта Athenaplus.kz/vacancy Форма - Запрос на вакансию"/>
                 <input type="hidden" name="_captcha" value="false"/>
                 <input type="text" name="_honey" style="display:none"/>
                 <input type="hidden" name="_template" value="table"/>
