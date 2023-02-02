a189eac055d45f619cb107d3db849f60dc55997f
AthenaPlus <polistovskiy.athenaplus@gmail.com>
2023-02-02 13:48:16 +0600

    Update Orderproject.vue

--- components/Home/Orderproject.vue
+++ components/Home/Orderproject.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="signInterview"
         class="text-white body-font relative min-h-screen bg-top bg-contain bg-no-repeat bg-[url('/img/bg_frame4_mob.png')] md:bg-center md:bg-cover  md:bg-[url('/img/bg_frame4.jpg')] z-0">
-        <div class="container px-5 py-16  md:py-24 mx-auto">
+        <div class="container px-5 py-16  md:py-24 mx-auto overflow-x-hidden">
             <div class="flex flex-col text-center w-full mb-12">
                 <h1 class="text-[32px] SuperTitle text-left md:mt-16 anime__left1">Есть проект?</h1>
                 <div class="flex items-center md:justify-end gap-2  md:mr-32 anime__right1">
