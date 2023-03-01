4ac68c92fdbb543fe66fd91283eb39c4ac5d3507
AthenaPlus <119859429+AthenaPlus@users.noreply.github.com>
2023-03-01 19:05:45 +0600

    Update

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,18 @@
 <template>
     <section>
         <div class="container px-5 py-24 mx-auto text-white">
-            <SlaiderSwiper/>
+            <!-- ================================================= -->
+            <div>
+                <h2>Filtered Posts</h2>
+                <ul>
+                    <!-- <li v-for="post in posts" :key="post.id">{{ post.title }}</li> -->
+                </ul>
+            </div>
+            <!-- ================================================= -->
         </div>
     </section>
 </template>
-<script>
+
+<script setup>
 
 </script>
