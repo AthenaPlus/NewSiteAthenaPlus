80221d6ddc61fe27abfc0394663e83eaad1fa760
AthenaPlus <119859429+AthenaPlus@users.noreply.github.com>
2023-03-03 14:13:21 +0600

    Update module content

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="text-white container px-5 py-24 mx-auto">
+        <ContentDoc />
+    </main>
+</template>
--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,71 @@
 <template>
-    <section>
-        <div class="container px-5 py-24 mx-auto text-white">
-            <!-- ================================================= -->
-            <div>
-                <h2>Filtered Posts</h2>
-                <ul>
-                    <!-- <li v-for="post in posts" :key="post.id">{{ post.title }}</li> -->
-                </ul>
-            </div>
-            <!-- ================================================= -->
+  <section>
+    <div class="container px-5 py-24 mx-auto text-white">
+      <!-- ================================================= -->
+      <div>
+        <h2>Filtered Posts</h2>
+        <pre> {{ data }}  </pre>
+
+        <div>
+          <div v-for="post in posts" :key="post.id">
+            <h2>{{ post.attributes.Title }}</h2>
+          </div>
         </div>
-    </section>
+
+      </div>
+      <!-- ================================================= -->
+    </div>
+  </section>
 </template>
 
 <script setup>
+const query = gql`
+  query getPosts {
+    posts {
+      data {
+        id
+        attributes {
+          Title
+          seo {
+            seoTitle
+          }
+          category {
+          data {
+            id
+            attributes {
+                Title
+            }
+          }
+        }
+        }
+      }
+    }
+  }
+`
+
+const { data } = await useAsyncQuery(query);
+
+</script>
+
+<script>
+export default {
+  setup() {
+    const posts = ref([]);
+
+    const { loading, error } = useAsyncQuery(query, {
+      onCompleted(data) {
+        posts.value = data.posts.data;
+      }
+    });
+
+    onMounted(() => {
+      if (!loading.value && !error.value) {
+        posts.value = data.posts.data;
+      }
+    });
+
+    return { posts, attributes  };
+  }
+};
 
 </script>
