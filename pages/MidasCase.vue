b0ca80efe3be4ebdc8a712bcf1f8ead58c2032d6
KobeWanAthena <113441500+KobeWanAthena@users.noreply.github.com>
2023-03-30 18:44:27 +0600

    Update MidasCase.vue

--- pages/MidasCase.vue
+++ pages/MidasCase.vue
@@ -59,7 +59,7 @@ export default {
         return {
             cases: [{
                 title: 'Midas Event agency',
-                desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                desc: 'Цель "Midas Event" - быть самым эффективным event-агентством в Казахстане.Команда работает по системному подходу, от разработки концепции, до конечной реализации.',
                 img: '/img/MidasFrame.png'
             }]
         }
