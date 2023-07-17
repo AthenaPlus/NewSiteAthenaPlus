e7f20316c6c4c83151c5039dab000f3de205da5b
poliweb <119859429+AthenaPlus@users.noreply.github.com>
2023-07-18 05:40:38 +0600

    Update vue-gtm.client.js

--- plugins/vue-gtm.client.js
+++ plugins/vue-gtm.client.js
@@ -3,7 +3,7 @@ import VueGtag from 'vue-gtag-next'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'G-7LY9Q3X2CT'
+      id: 'G-LHJSDCR75E'
     }
   })
 })
