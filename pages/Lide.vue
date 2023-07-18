85e00a59261864655d281a709c904d88537c5aad
poliweb <119859429+AthenaPlus@users.noreply.github.com>
2023-07-18 11:05:46 +0600

    Update Lide.vue

--- pages/Lide.vue
+++ pages/Lide.vue
@@ -1,20 +1,36 @@
 <template>
-    <section>
-        <div class="container px-5 py-24 mx-auto text-white">
-            <form @submit="submitForm" method="post">
-                <label for="name">Имя:</label>
-                <input class="text-black" type="text" id="NAME" v-model="name" required>
+  <section>
+    <div class="container px-5 py-24 mx-auto text-white">
+      <form @submit="submitForm" method="post">
+        <div class="hidden md:flex flex-wrap justify-start  gap-5 mb-10">
+          <label for="app" @click="clickButton1" class="btn--label text-white py-3 cursor-pointer"
+            :class="[isClickButton1 ? 'btn--click' : '']"> Приложение</label>
+          <input type="radio" id="app" name="project_type" value="Приложение" class="w-0" />
 
-                <label for="email">Email:</label>
-                <input class="text-black" type="email" id="EMAIL" v-model="email" required>
+          <label for="Website" @click="clickButton2" class="btn--label text-white py-3 cursor-pointer"
+            :class="[isClickButton2 ? 'btn--click' : '']"> Сайт</label>
+          <input type="radio" id="Website" name="project_type" value="Сайт" class="w-0" />
 
-                <label for="phone">Телефон:</label>
-                <input class="text-black" type="tel" id="PHONE" v-model="phone" required>
-
-                <button type="submit">Отправить</button>
-            </form>
+          <label for="CRM" @click="clickButton3" class="btn--label text-white py-3 cursor-pointer"
+            :class="[isClickButton3 ? 'btn--click' : '']">CRM-система</label>
+          <input type="radio" id="CRM" name="project_type" value="CRM-система" class="w-0" />
+          <label for="marketing" @click="clickButton4" class="btn--label text-white py-3 cursor-pointer"
+            :class="[isClickButton4 ? 'btn--click' : '']">Маркетинг</label>
+          <input type="radio" id="marketing" name="project_type" value="Маркетинг" class="w-0" />
         </div>
-    </section>
+        <label for="name">Имя:</label>
+        <input class="text-black" type="text" id="NAME" v-model="name" required>
+
+        <label for="email">Email:</label>
+        <input class="text-black" type="email" id="EMAIL" v-model="email" required>
+
+        <label for="phone">Телефон:</label>
+        <input class="text-black" type="tel" id="PHONE" v-model="phone" required>
+
+        <button type="submit">Отправить</button>
+      </form>
+    </div>
+  </section>
 </template>
   
 <script>
@@ -32,17 +48,17 @@ export default {
 
       const formData = {
         fields: {
-            NAME: this.name,
-            EMAIL:[
-                {
-                    "VALUE": this.email
-                }
-            ],
-            PHONE: [
-                {
-                    "VALUE": this.phone
-                }
-            ],
+          NAME: this.name,
+          EMAIL: [
+            {
+              "VALUE": this.email
+            }
+          ],
+          PHONE: [
+            {
+              "VALUE": this.phone
+            }
+          ],
         },
       };
 
