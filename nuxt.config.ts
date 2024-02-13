1cdd959d88416b2e763b2517ebca74d3601a7923
AthenaPlus <119859429+AthenaPlus@users.noreply.github.com>
2024-02-13 20:32:36 +0600

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/apollo', '@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-svgo', 'nuxt-headlessui', '@fullpage/nuxt-fullpage', '@nuxtjs/sitemap'],
+    modules: ['@nuxtjs/apollo', '@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-svgo', 'nuxt-headlessui', '@fullpage/nuxt-fullpage'],
 
     css: ['animate.css'],
 
